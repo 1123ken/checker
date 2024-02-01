9c773bdce9b02a9c545eab523f17fa43125e4f1e
1123ken <red_00.1123ken@live.jp>
2024-02-01 10:12:22 +0900

    2/1

--- src/main/java/dao/package-info.java
+++ src/main/java/dao/package-info.java
@@ -1,8 +1,18 @@
 /**
+ * daoフォルダ<br>
  * 
+ * ・CharacterDAO<br>
+ * 　キャラクター名、idの検索、取得をデータベースへ行う処理のDAO<br>
+ * 
+ * ・SaveDataDAO<br>
+ * 　home.jspで入力した内容をpointテーブルに情報をデータベースに登録するDAO<br>
+ * 
+ * ・TitleDAO<br>
+ * 　タイトルの検索、登録、タイトル＋キャラクターの登録、タイトルIDからタイトル名を検索する<br>
+ * 　タイトル名からキャラクターを検索する等タイトルに絡む処理をデータベースに行うDAO<br>
  */
 /**
- * @author 7d03
+ * @author 馬場 健太朗
  *
  */
 package dao;
